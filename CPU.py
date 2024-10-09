9ccd52034254031648d7ccde58a0b7a32dff23cf
jondavidson <jondavidson@users.noreply.github.com>
2024-10-09 08:25:59 +0100

    Update CPU.py

--- CPU.py
+++ CPU.py
@@ -19,3 +19,28 @@ for i in range(num_workers):
     cluster.scale(n=i+1, env=env)
     # Register the CPU affinity function
     client.register_worker_callbacks(set_cpu_affinity)
+
+
+from dask.distributed import Client, SpecCluster, Nanny
+import os
+
+def set_cpu_affinity():
+    cpu_core = int(os.environ.get('DASK_CPU_AFFINITY', -1))
+    if cpu_core >= 0:
+        os.sched_setaffinity(0, {cpu_core})
+
+num_workers = 8  # Adjust based on your number of physical cores
+
+worker_specs = {
+    f'worker-{i}': {
+        'cls': Nanny,
+        'options': {
+            'env': {'DASK_CPU_AFFINITY': str(i)},
+            'plugins': [set_cpu_affinity],
+        },
+    }
+    for i in range(num_workers)
+}
+
+cluster = SpecCluster(worker_specs)
+client = Client(cluster)
